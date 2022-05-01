24b2c02dd158ab3816995d1c8ba49c34c0e3939b
Akshay-59 <103379131+Akshay-59@users.noreply.github.com>
2022-05-02 00:58:39 +0530

    Add files via upload

---
+++ Angular/product.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  pname:string ="";
+  price:number=0;
+  qty:number =0;
+  Total:number=0;
+  Color:string="";
+
+  GetTotal()
+  {
+    this.Total = (this.price*this.qty);
+    if(this.Total>10000)
+    {
+      this.Color="green"
+
+    }
+    else{
+      this.Color="red"
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
