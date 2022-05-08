a63ed63fe6df690d93d82abc9271d9e1d6c9213a
Akshay-59 <103379131+Akshay-59@users.noreply.github.com>
2022-05-09 00:43:17 +0530

    Add files via upload

---
+++ Angular/Day- 1/Task 1/employee.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent  {
+
+  public empId:number = 54654564;
+  public empName:string ="Stephen";
+  public empJob:string ="Manger";
+  public empSalary:number = 75000;
+  public empDep:number = 10;
+
+}
