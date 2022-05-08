6605c3183856654091957e899a86a0910a8a2754
Akshay-59 <103379131+Akshay-59@users.noreply.github.com>
2022-05-09 00:25:43 +0530

    Add files via upload

---
+++ Angular/Day-7/Shadow directive/shadow.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appShadow]'
+})
+export class ShadowDirective {
+
+  @Input() 
+appShadow:string ="";
+
+
+constructor(private elem: ElementRef, private renderer: Renderer2) { }
+
+shadow:string = "text-shadow: 2px 3px 1px;";
+ngOnInit() {
+  if(this.appShadow == "")
+     {
+        this.appShadow = "red";
+     }  
+     this.elem.nativeElement.style = this.shadow;
+     this.elem.nativeElement.style.color = this.appShadow;
+}
+
+}
