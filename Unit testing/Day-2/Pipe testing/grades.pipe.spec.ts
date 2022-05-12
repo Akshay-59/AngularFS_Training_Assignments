2e837589c3733630b5ea359844ed7e98e51abd43
Akshay-59 <103379131+Akshay-59@users.noreply.github.com>
2022-05-12 13:14:30 +0530

    Add files via upload

---
+++ Unit testing/Day-2/Pipe testing/grades.pipe.spec.ts
@@ -0,0 +1,51 @@
+import { GradesPipe } from './grades.pipe';
+
+describe('GradesPipe', () => {
+
+  let grades:GradesPipe;
+  it('create an instance', () => {
+    const pipe = new GradesPipe();
+    expect(pipe).toBeTruthy();
+  });
+  
+  beforeEach(() => {
+    const grade = new GradesPipe();
+  });
+
+
+  it('create an instance', () => {
+    expect(grade).toBeTruthy();
+  });
+
+  
+  // 2. Test grade  Valid Value
+  it('should return "Outstanding" when input is  "1" ', () => {
+    const result:string  = grade.transform(1);
+    expect(result).toBe("Outstanding");
+  });
+
+  // 3. Test grade Valid Value
+  it('should return "Excellent" when input is  "2" ', () => {
+    const result:string  = grade.transform(2);
+    expect(result).toBe("Excellent");
+  });
+
+
+  // 4. Test grade Valid Value
+  it('should return "Good" when input is  "4" ', () => {
+    const result:string  = grade.transform(4);
+    expect(result).toBe("Good");
+  });
+
+  // 4. Test grade Invalid Value 
+  it('should return "" when input value is invalid : 6 ', () => {
+    const result:string  = grade.transform(6);
+    expect(result).toBe("");
+  });
+
+  // 4. Test grade for Invalid Value
+  it('should return "" when input value is invalid : 0 ', () => {
+    const result:string  = grade.transform(0);
+    expect(result).toBe("");
+  });
+});
